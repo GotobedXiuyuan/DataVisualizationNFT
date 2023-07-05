f2e4a8206523a8d0cc231a859faf5a4e5d52aec2
GotobedXiuyuan <78220045+GotobedXiuyuan@users.noreply.github.com>
2023-07-05 13:26:59 -0400

    Update blockchain_interact.py

--- blockchain_interact.py
+++ blockchain_interact.py
@@ -1,44 +1,55 @@
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
 import sys
 import json
+import time
 
-# you need to replace 'infura_project_id' with your actual Infura Project ID
-infura_url = "https://mainnet.infura.io/v3/infura_project_id" 
-web3 = Web3(Web3.HTTPProvider(infura_url))
+local_blockchain_url = "http://127.0.0.1:8545"
+web3 = Web3(Web3.HTTPProvider(local_blockchain_url))
+web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-# replace 'private_key' with your Ethereum account private key
-private_key = "0x2f75ee973c8a5da24942929bcb98133b0538753ee265feb9f95086ead309983d"
-
-# replace 'contract_address' with the address of your deployed smart contract
-contract_address = "0x5BeBC0B388aAA2075c3189420bdBFCD84c9cA463"
-
-# replace 'abi_json_file' with the path to your contract's ABI JSON file
-abi_json_file = 'abi.json'
+contract_address = Web3.to_checksum_address("0xe466b03c36b7a39eae227c994788c92722992947")
+abi_json_file = '/Users/wuxiuyuan/build/contracts/VisualizationRegistry.json'
 
 def load_abi(abi_file):
     with open(abi_file) as f:
         abi = json.load(f)
-    return abi
+    return abi['abi']
 
-def register_visualization(image_hash):
+def register_visualization(account_address, visualization_name, image_hash):
     abi = load_abi(abi_json_file)
     contract = web3.eth.contract(address=contract_address, abi=abi)
     
-    nonce = web3.eth.getTransactionCount(web3.eth.defaultAccount)
+    nonce = web3.eth.get_transaction_count(account_address)
 
-    txn = contract.functions.registerVisualization(image_hash).buildTransaction({
+    txn_function = contract.functions.registerVisualization(account_address, visualization_name, bytes.fromhex(image_hash))
+    txn ={
         'chainId': 1,
-        'gas': 70000,
-        'gasPrice': web3.toWei('1', 'gwei'),
+        'gas': 100000,
+        'gasPrice':1,
         'nonce': nonce,
-    })
+    }
 
-    signed_txn = web3.eth.account.signTransaction(txn, private_key=private_key)
 
-    tx_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    signed_txn = web3.eth.account.sign_transaction(txn, private_key=private_key)
+
+    tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
     
-    return tx_hash.hex()
+    print(f'Transaction hash: {tx_hash.hex()}')
+
+    print('Waiting for transaction to be mined...')
+    while True:
+        txn_receipt = web3.eth.get_transaction_receipt(tx_hash)
+        if txn_receipt is not None:
+            break
+        time.sleep(3)  # wait before retrying
+
+    print(f'Transaction status: {"Successful" if txn_receipt["status"] == 1 else "Failed"}')
 
 if __name__ == "__main__":
     image_hash = sys.argv[1]
-    print(register_visualization(image_hash))
+    private_key = "0x4985ebabcf2eb9555f2c557b83dc5c57f51c49ad77d6fcf4ba66a54d4f4b38c9"
+    account = web3.eth.account.from_key(private_key)
+    visualization_name = "my visualization"
+    register_visualization(account_address=account.address, visualization_name=visualization_name, image_hash=image_hash)
+
